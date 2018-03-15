ab2c32b89edc6f1f29cf04f2174233f5202ff237
Hardik Rana <hardikrana276@gmail.com>
2018-03-15 21:19:55 +0530

    Update forms.py

--- page/forms.py
+++ page/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from django.contrib.auth.models import User
 from page.models import Victim,Lawyer,Police
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth import authenticate, login,get_user_model,logout
 
 #from page.models import UserProfile
 
@@ -48,7 +48,29 @@ class PoliceSignUpForm(UserCreationForm):
         ]
 
 
+#class VictimCaseForm(UserCreationForm):
+User = get_user_model()
 
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self, *args,**kwargs):
+        username = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password")
+        #user = authenticate(username=username,password=password)
+        #user_qs = User.objects.filter(username=username)
+        #if user_qs.count() == 1:
+           # user = user_qs.first()
+        if username and password:
+            user = authenticate(username=username,password=password)
+            if not user:
+                raise forms.ValidationError("This User does not exist")
+            if not user.check_password(password):
+                raise forms.ValidationError("Incorrect Password")
+            if not user.is_active:
+                raise forms.ValidationError("This user is not longer active")
+        return super(UserLoginForm,self).clean(*args,**kwargs)
 
 
 class RegistrationForm(UserCreationForm):
