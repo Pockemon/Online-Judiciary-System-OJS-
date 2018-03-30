1de50dda2e199fc73c45ddc2929c82b3de5f122d
Hardik Rana <hardikrana276@gmail.com>
2018-03-30 18:02:05 +0530

    final build

--- page/urls.py
+++ page/urls.py
@@ -82,6 +82,12 @@ urlpatterns = [
 
    path('edit_profile_police',views.edit_profile_police,name="edit_profile_police"),
 
+   
+   #path('victim_home',views.victim_home,name="victim_home"),
+
+   #path('lawyer_home',views.lawyer_home,name="lawyer_home"),
+
+   #path('police_home',views.police_home,name="police_home"),
 
    #"just for debugging"
    #path('profile/edit',views.edit_profile,name="edit_profile"),
@@ -90,6 +96,7 @@ urlpatterns = [
    #path('profile/reset_password/done',password_reset_done,name='password_reset_done'),
    #path('profile/reset_password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)',password_reset_confirm,name='password_reset_confirm'),
    #path('profile/reset_password/complete',password_reset_complete,name='password_reset_comlete'),
+
 ]
 
 
